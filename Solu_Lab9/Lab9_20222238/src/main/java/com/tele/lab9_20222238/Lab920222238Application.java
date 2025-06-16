5a458137de35856aaad8ef226d6074f00a188143
Polar0706 <a20222238@pucp.edu.pe>
2025-06-16 10:52:18 -0500

    inicio

---
+++ Solu_Lab9/Lab9_20222238/src/main/java/com/tele/lab9_20222238/Lab920222238Application.java
@@ -0,0 +1,13 @@
+package com.tele.lab9_20222238;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920222238Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920222238Application.class, args);
+    }
+
+}
---
+++ Solu_Lab9/Lab9_20222238/src/test/java/com/tele/lab9_20222238/Lab920222238ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tele.lab9_20222238;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920222238ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
